cdbdb0b2d572fa958d645de97f867c0b252478f7
christopherhedley <christopher.g.hedley@knights.ucf.edu>
2018-11-08 22:56:38 -0500

    Push of finished extended tutorial

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public int sceneToLoad;
+
+    public Text ScoreText;
+    private int score;
+    public Text restartText;
+    public Text gameoverText;
+
+    private bool gameOver;
+    private bool restart;
+
+
+    void Start()
+    {
+        score = 0;
+        UpdateScore();
+
+        gameOver = false;
+        restart = false;
+        gameoverText.text = "";
+        restartText.text = "";
+
+        StartCoroutine (SpawnWaves());
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range (0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' to Restart or 'Esc' To Quit";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        ScoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameoverText.text = "Game Over";
+        gameOver = true;
+    }
+
+}
